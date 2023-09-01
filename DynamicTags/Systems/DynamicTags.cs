b857e73d43f0dbd227f335ab5d72382d05979471
Phoenix <thephoenixproject208@gmail.com>
2023-09-01 23:51:47 +0100

    Update to 13.2

--- DynamicTags/Systems/DynamicTags.cs
+++ DynamicTags/Systems/DynamicTags.cs
@@ -16,167 +16,205 @@ using PluginAPI.Events;
 
 namespace DynamicTags.Systems
 {
-    public class DynamicTags
-    {
-        [CommandHandler(typeof(RemoteAdminCommandHandler))]
-        [CommandHandler(typeof(ClientCommandHandler))]
-        public class DynamicTagCommand : ICommand
-        {
-            public string Command => "dynamictag";
+	public class DynamicTags
+	{
+		[CommandHandler(typeof(RemoteAdminCommandHandler))]
+		[CommandHandler(typeof(ClientCommandHandler))]
+		public class DynamicTagCommand : ICommand
+		{
+			public string Command => "dynamictag";
 
-            public string[] Aliases { get; } = { "dtag", "dt" };
+			public string[] Aliases { get; } = { "dtag", "dt" };
 
-            public string Description => "Shows your dynamic tag";
+			public string Description => "Shows your dynamic tag";
 
-            public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
-            {
-                if (sender is PlayerCommandSender pSender)
-                {
-                    if (Tags.ContainsKey(pSender.ReferenceHub.characterClassManager.UserId))
-                    {
-                        TagData data = Tags[pSender.ReferenceHub.characterClassManager.UserId];
+			public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+			{
+				if (sender is PlayerCommandSender pSender)
+				{
+					if (Tags.ContainsKey(pSender.ReferenceHub.characterClassManager.UserId))
+					{
+						TagData data = Tags[pSender.ReferenceHub.characterClassManager.UserId];
 
-                        //This is to stop situations where users have locally assigned perms but gets overridden by NULL perms from the external server.
-                        if (!string.IsNullOrEmpty(data.Group))
-                        {
-                            pSender.ReferenceHub.serverRoles.SetGroup(ServerStatic.GetPermissionsHandler().GetGroup(data.Group), true);
-                            pSender.ReferenceHub.serverRoles.RemoteAdmin = true;
-                            pSender.ReferenceHub.serverRoles.RemoteAdminMode = ServerRoles.AccessMode.LocalAccess;
+						//This is to stop situations where users have locally assigned perms but gets overridden by NULL perms from the external server.
+						if (!string.IsNullOrEmpty(data.Group))
+						{
+							pSender.ReferenceHub.serverRoles.SetGroup(ServerStatic.GetPermissionsHandler().GetGroup(data.Group), true);
+							pSender.ReferenceHub.serverRoles.RemoteAdmin = true;
+							pSender.ReferenceHub.serverRoles.RemoteAdminMode = ServerRoles.AccessMode.LocalAccess;
 
-                            if (data.Perms != 0)
-                                pSender.ReferenceHub.serverRoles.Permissions = data.Perms;
+							if (data.Perms != 0)
+								pSender.ReferenceHub.serverRoles.Permissions = data.Perms;
 
-                        }
+						}
 
-                        pSender.ReferenceHub.serverRoles.SetText(data.Tag);
-                        pSender.ReferenceHub.serverRoles.SetColor(data.Colour);
+						pSender.ReferenceHub.serverRoles.SetText(data.Tag);
+						pSender.ReferenceHub.serverRoles.SetColor(data.Colour);
 
 
 
-                        response = "Dynamic tag loaded: " + data.Tag;
-                        return true;
-                    }
-                    response = "You have no tag";
-                    return true;
-                }
+						response = "Dynamic tag loaded: " + data.Tag;
+						return true;
+					}
+					response = "You have no tag";
+					return true;
+				}
 
-                response = "This command must be run as a player command";
-                return false;
-            }
-        }
+				response = "This command must be run as a player command";
+				return false;
+			}
+		}
 
-        [CommandHandler(typeof(RemoteAdminCommandHandler))]
-        public class DynamicTagListCommand : ICommand
-        {
-            public string Command => "dynamictaglist";
+		[CommandHandler(typeof(RemoteAdminCommandHandler))]
+		public class DynamicTagListCommand : ICommand
+		{
+			public string Command => "dynamictaglist";
 
-            public string[] Aliases { get; } = { "dtaglist", "dtl" };
+			public string[] Aliases { get; } = { "dtaglist", "dtl" };
 
-            public string Description => "Lists all dynamic tags";
+			public string Description => "Lists all dynamic tags";
 
-            public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
-            {
-                if (sender is PlayerCommandSender pSender)
-                {
-                    if (sender.CheckPermission(PlayerPermissions.PermissionsManagement))
-                    {
-                        List<string> tags = new List<string>();
+			public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+			{
+				if (sender is PlayerCommandSender pSender)
+				{
+					if (sender.CheckPermission(PlayerPermissions.PermissionsManagement))
+					{
+						List<string> tags = new List<string>();
 
-                        foreach(var tag in Tags)
-                        {
-                            tags.Add($"{tag.Key} | {tag.Value.Tag}");
-                        }
+						foreach (var tag in Tags)
+						{
+							tags.Add($"{tag.Key} | {tag.Value.Tag}");
+						}
 
-                        response = string.Join("\n", tags);
-                        return true;
-                    }
-                    response = "You cannot run this command";
-                    return true;
-                }
+						response = string.Join("\n", tags);
+						return true;
+					}
+					response = "You cannot run this command";
+					return true;
+				}
 
-                response = "This command must be run as a player command";
-                return false;
-            }
-        }
+				response = "This command must be run as a player command";
+				return false;
+			}
+		}
 
-        public static Dictionary<string, TagData> Tags = new Dictionary<string, TagData>();
+		[CommandHandler(typeof(RemoteAdminCommandHandler))]
+		public class UpdateDynamicTagsCommand : ICommand
+		{
+			public string Command => "dynamictagupdate";
 
-        [PluginEvent(ServerEventType.WaitingForPlayers)]
-        public async void OnWaitingForPlayers()
-        {
-            try
-            {
-                //Clears all previous tags held by the server (Prevents players from keeping tags when they have been removed from the external server).
-                Tags.Clear();
+			public string[] Aliases { get; } = { "dtagupdate", "dtu" };
 
-                var response = await Extensions.Get(Plugin.Config.ApiEndpoint + "games/gettags");
+			public string Description => "Updates all dynamic tags";
 
-                //Log.Info(await response.Content.ReadAsStringAsync());
+			public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+			{
+				if (sender is PlayerCommandSender pSender)
+				{
+					if (sender.CheckPermission(PlayerPermissions.PermissionsManagement))
+					{
+						UpdateTags(true);
 
-                var tags = JsonConvert.DeserializeObject<TagData[]>(await response.Content.ReadAsStringAsync());
+						response = "Dynamic tags updated";
+						return true;
+					}
+					response = "You cannot run this command";
+					return true;
+				}
 
-                foreach (var a in tags)
-                {
-                    if (a.UserID.StartsWith("7656"))
-                        a.UserID = $"{a.UserID}@steam";
-                    else if (ulong.TryParse(a.UserID, out ulong result))
-                        a.UserID = $"{a.UserID}@discord";
-                    else
-                        a.UserID = $"{a.UserID}@northwood";
+				response = "This command must be run as a player command";
+				return false;
+			}
+		}
 
-                    //Adds the tags to the tag list.
-                    Tags.Add(a.UserID, a);
-                }
+		public static Dictionary<string, TagData> Tags = new Dictionary<string, TagData>();
 
-                Log.Info($"{Tags.Count} tags loaded");
-            }
-            catch (Exception e)
-            {
-                Log.Error(e.ToString());
-            }
-        }
+		[PluginEvent(ServerEventType.WaitingForPlayers)]
+		public async void OnWaitingForPlayers()
+		{
+			UpdateTags();
+		}
 
-        [PluginEvent(ServerEventType.PlayerCheckReservedSlot)]
-        public PlayerCheckReservedSlotCancellationData OnReservedSlotCheck(PlayerCheckReservedSlotEvent args)
-        {
-            if (args.HasReservedSlot)
-                return PlayerCheckReservedSlotCancellationData.LeaveUnchanged();
+		public static async void UpdateTags(bool ForceUpdate = false)
+		{
+			try
+			{
+				//Clears all previous tags held by the server (Prevents players from keeping tags when they have been removed from the external server).
+				Tags.Clear();
 
-            else if (args.Userid.ToLower().Contains("northwood") && Plugin.Config.AutomaticNorthwoodReservedSlot)
-            {
-                Log.Info($"Reserved slot bypass for {args.Userid} (Northwood ID detected)");
-                return PlayerCheckReservedSlotCancellationData.BypassCheck();
-            }
-            else if (Tags.ContainsKey(args.Userid) && Tags[args.Userid].ReservedSlot)
-            {
-                Log.Info($"Reserved slot bypass for {args.Userid} (Dynamic Tag)");
-                return PlayerCheckReservedSlotCancellationData.BypassCheck();
-            }
+				var response = await Extensions.Get(Plugin.Config.ApiEndpoint + "games/gettags");
 
-            else return PlayerCheckReservedSlotCancellationData.LeaveUnchanged();
-        }
+				var tags = JsonConvert.DeserializeObject<TagData[]>(await response.Content.ReadAsStringAsync());
 
-        [PluginEvent(ServerEventType.PlayerJoined)]
-        public void OnPlayerJoin(PlayerJoinedEvent args)
-        {
-            //Checks if the user has a tag
-            if (Tags.ContainsKey(args.Player.UserId))
-            {
-                TagData data = Tags[args.Player.UserId];
+				foreach (var a in tags)
+				{
+					if (a.UserID.StartsWith("7656"))
+						a.UserID = $"{a.UserID}@steam";
+					else if (ulong.TryParse(a.UserID, out ulong result))
+						a.UserID = $"{a.UserID}@discord";
+					else
+						a.UserID = $"{a.UserID}@northwood";
 
-                //This is to stop situations where users have locally assigned perms but gets overridden by NULL perms from the external server.
-                if (!string.IsNullOrEmpty(data.Group))
-                    args.Player.ReferenceHub.serverRoles.SetGroup(ServerStatic.GetPermissionsHandler().GetGroup(data.Group), true);
+					//Adds the tags to the tag list.
+					Tags.Add(a.UserID, a);
+				}
 
-                args.Player.ReferenceHub.serverRoles.SetText(data.Tag);
-                args.Player.ReferenceHub.serverRoles.SetColor(data.Colour);
+				Log.Info($"{Tags.Count} tags loaded");
 
-                if (data.Perms != 0)
-                    args.Player.ReferenceHub.serverRoles.Permissions = data.Perms;
+				foreach (var plr in Player.GetPlayers())
+					if (Tags.ContainsKey(plr.UserId))
+						SetDynamicTag(plr, Tags[plr.UserId]);			
+			}
+			catch (Exception e)
+			{
+				Log.Error(e.ToString());
+			}
+		}
 
-                args.Player.SendConsoleMessage("Dynamic tag loaded: " + data.Tag);
-                Log.Info($"Tag found for {args.Player.UserId}: {data.Tag}");
-            }
-        }
-    }
+		[PluginEvent(ServerEventType.PlayerCheckReservedSlot)]
+		public PlayerCheckReservedSlotCancellationData OnReservedSlotCheck(PlayerCheckReservedSlotEvent args)
+		{
+			if (args.HasReservedSlot)
+				return PlayerCheckReservedSlotCancellationData.LeaveUnchanged();
+
+			else if (args.Userid.ToLower().Contains("northwood") && Plugin.Config.AutomaticNorthwoodReservedSlot)
+			{
+				Log.Info($"Reserved slot bypass for {args.Userid} (Northwood ID detected)");
+				return PlayerCheckReservedSlotCancellationData.BypassCheck();
+			}
+			else if (Tags.ContainsKey(args.Userid) && Tags[args.Userid].ReservedSlot)
+			{
+				Log.Info($"Reserved slot bypass for {args.Userid} (Dynamic Tag)");
+				return PlayerCheckReservedSlotCancellationData.BypassCheck();
+			}
+
+			else return PlayerCheckReservedSlotCancellationData.LeaveUnchanged();
+		}
+
+		[PluginEvent(ServerEventType.PlayerJoined)]
+		public void OnPlayerJoin(PlayerJoinedEvent args)
+		{
+			//Checks if the user has a tag
+			if (Tags.ContainsKey(args.Player.UserId))
+				SetDynamicTag(args.Player, Tags[args.Player.UserId]);
+
+		}
+
+		public static void SetDynamicTag(Player player, TagData data)
+		{
+			//This is to stop situations where users have locally assigned perms but gets overridden by NULL perms from the external server.
+			if (!string.IsNullOrEmpty(data.Group))
+				player.ReferenceHub.serverRoles.SetGroup(ServerStatic.GetPermissionsHandler().GetGroup(data.Group), true);
+
+			player.ReferenceHub.serverRoles.SetText(data.Tag);
+			player.ReferenceHub.serverRoles.SetColor(data.Colour);
+
+			if (data.Perms != 0)
+				player.ReferenceHub.serverRoles.Permissions = data.Perms;
+
+			player.SendConsoleMessage("Dynamic tag loaded: " + data.Tag);
+			Log.Info($"Tag found for {player.UserId}: {data.Tag}");
+		}
+	}
 }
